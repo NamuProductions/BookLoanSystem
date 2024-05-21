c9e7db74998dfac1c0c317e92d8eb063b1a8c073
ryan <tvmuixi@hotmail.com>
2024-05-21 20:29:41 +0100

    vamos a por el when

--- Tests/Action/User/LoginUserActionTest.php
+++ Tests/Action/User/LoginUserActionTest.php
@@ -5,7 +5,7 @@ namespace Action\User;
 use PHPUnit\Framework\TestCase;
 use App\Action\User\LoginUserAction;
 use App\Domain\Repository\UserRepository;
-
+use App\Domain\Model\User;
 
 class LoginUserActionTest extends TestCase
 {
@@ -16,6 +16,26 @@ class LoginUserActionTest extends TestCase
         parent::setUp();
         $userRepository = $this->createMock(UserRepository::class);
         $this->sut = new LoginUserAction($userRepository);
+    }
+
+    public function it_should_login_a_registered_User(): void
+    {
+        // given
+        $userName = 'testUser';
+        $password = 'testPassword';
+        $userPassword = user_password($password, PASSWORD_DEFAULT); // todo: pensar un mejor nombre por que puede inducir a error
+
+        $user = new User($userName, 'testUser@example.com', $userPassword);
+
+        $this->userRepository
+            ->expects($this->once())
+            ->methode('findByUserName')
+            ->with($userName)
+            ->willReturn($user);
+
+        // when
+
+
     }
 
 }
