84867942e31bba3b9d2e7f8c18a68348b31060be
ryan <tvmuixi@hotmail.com>
2024-05-27 13:27:31 +0100

    pasado todo bien al constructor y invocado metodo mágico __ para construct por que sin mágia no va.

--- src/Domain/Model/Book.php
+++ src/Domain/Model/Book.php
@@ -4,29 +4,36 @@ namespace App\Domain\Model;
 
 readonly class Book
 {
-    protected string $title;
-    protected string $author;
-    protected string $year;
-    protected string $idNumber;
+    public function __construct(
+        private string $title,
+        private string $author,
+        private string $year,
+        private string $idNumber
+)
+{
+}
 
-    public function construct(
+public
+function getTitle(): string
+{
+    return $this->title;
+}
 
-) {}
+public
+function getAuthor(): string
+{
+    return $this->author;
+}
 
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-    public function getAuthor(): string
-    {
-        return $this->author;
-    }
-    public function getYear(): string
-    {
-        return $this->year;
-    }
-    public function getIdNumber(): string
-    {
-        return $this->idNumber;
-    }
+public
+function getYear(): string
+{
+    return $this->year;
+}
+
+public
+function getIdNumber(): string
+{
+    return $this->idNumber;
+}
 }
