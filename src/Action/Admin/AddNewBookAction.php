803a15f674b7b19472b13b3bc33f8a0c242648f7
ryan <tvmuixi@hotmail.com>
2024-05-27 13:34:59 +0100

    implemented exception if one parameter is missing when creating a new book

--- src/Action/Admin/AddNewBookAction.php
+++ src/Action/Admin/AddNewBookAction.php
@@ -5,6 +5,7 @@ namespace App\Action\Admin;
 
 use App\Domain\Model\Book;
 use App\Domain\Repository\BookRepository;
+use PHPUnit\Event\InvalidArgumentException;
 
 readonly class AddNewBookAction
 {
@@ -15,6 +16,10 @@ readonly class AddNewBookAction
 
     public function __invoke(string $title, string $author, string $year, string $idNumber): void
     {
+        if(empty($title) || empty($author) || empty($year) || empty($idNumber)) {
+            throw new InvalidArgumentException('Title, author, year and idNumber are required');
+        }
+
         $book = new Book($title, $author, $year, $idNumber);
         $this->bookRepository->save($book);
     }
