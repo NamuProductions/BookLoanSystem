1cad5eea40658c09877f114ba00ffe57733a8586
ryan <tvmuixi@hotmail.com>
2024-05-27 20:16:38 +0100

    starting list loan requests test

---
+++ Tests/Action/Admin/ListLoanRequestsActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Action\Admin;
+
+
+use App\Domain\Repository\LoanRepository;
+use PHPUnit\Framework\TestCase;
+use App\Action\Admin\ListLoanRequestsAction;
+use PHPUnit\Framework\MockObject\Exception;
+
+
+class ListLoanRequestsActionTest extends TestCase
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->LoanRepository = $this->createMock(LoanRepository::class);
+        $this->sut = new ListLoanRequestsAction($this->LoanRepository);
+    }
+}
