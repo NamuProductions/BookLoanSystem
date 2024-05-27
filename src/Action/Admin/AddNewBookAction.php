30c0fd4d1d4ab4a7fc5e8a5e1163e6b4abe55d83
ryan <tvmuixi@hotmail.com>
2024-05-27 12:48:46 +0100

    implemented how to add a new book

--- src/Action/Admin/AddNewBookAction.php
+++ src/Action/Admin/AddNewBookAction.php
@@ -3,14 +3,19 @@
 namespace App\Action\Admin;
 
 
-class AddNewBookAction
+use App\Domain\Model\Book;
+use App\Domain\Repository\BookRepository;
+
+readonly class AddNewBookAction
 {
 
-    public function __construct($bookRepository)
+    public function __construct(private BookRepository $bookRepository)
     {
     }
 
-    public function __invoke(string $title, string $author, string $year, string $idNumber)
+    public function __invoke(string $title, string $author, string $year, string $idNumber): void
     {
+        $book = new Book($title, $author, $year, $idNumber);
+        $this->bookRepository->save($book);
     }
 }
