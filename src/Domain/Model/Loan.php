1bad549c2a7316d3746b023a85c54554f0a2fba3
ryan <tvmuixi@hotmail.com>
2024-05-27 20:46:04 +0100

    implemented Loan

--- src/Domain/Model/Loan.php
+++ src/Domain/Model/Loan.php
@@ -4,14 +4,30 @@ namespace App\Domain\Model;
 
 class Loan
 {
+    public function __construct(
+        private string $userName,
+        private string $bookTitle,
+        private string $startDate,
+        private string $endDate
+    ) {}
 
-    /**
-     * @param string $string
-     * @param string $string1
-     * @param string $string2
-     * @param string $string3
-     */
-    public function __construct(string $string, string $string1, string $string2, string $string3)
+    public function getUserName(): string
     {
+        return $this->userName;
+    }
+
+    public function getBookTitle(): string
+    {
+        return $this->bookTitle;
+    }
+
+    public function getStartDate(): string
+    {
+        return $this->startDate;
+    }
+
+    public function getEndDate(): string
+    {
+        return $this->endDate;
     }
 }
