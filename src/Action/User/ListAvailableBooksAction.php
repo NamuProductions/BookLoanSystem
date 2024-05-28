60b9d57011d633626b740508f1e1430281760992
ryan <tvmuixi@hotmail.com>
2024-05-28 16:01:52 +0100

    invoke correcto

--- src/Action/User/ListAvailableBooksAction.php
+++ src/Action/User/ListAvailableBooksAction.php
@@ -2,10 +2,16 @@
 
 namespace App\Action\User;
 
-/**
- * @method __invoke()
- */
-class ListAvailableBooksAction
-{
+use App\Domain\Repository\BookRepository;
 
+readonly class ListAvailableBooksAction
+{
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
+    public function __invoke(): array
+    {
+        return $this->bookRepository->findAvailableBooks();
+    }
 }
