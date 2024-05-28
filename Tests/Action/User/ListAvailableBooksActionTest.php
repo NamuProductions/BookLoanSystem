a6e69fbf76e49cf9bb656c5a501dc49ec59a67ed
ryan <tvmuixi@hotmail.com>
2024-05-28 15:49:52 +0100

    test for Available Books for the user

---
+++ Tests/Action/User/ListAvailableBooksActionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Action\User;
+
+use App\Action\User\ListAvailableBooksAction;
+use App\Domain\Model\Book;
+use App\Domain\Repository\BookRepository;
+use PHPUnit\Framework\TestCase;
+
+class ListAvailableBooksActionTest extends TestCase
+{
+    private BookRepository $bookRepository;
+    private ListAvailableBooksAction $sut;
+
+    public function test_it_should_list_all_available_books(): void
+    {
+        $availableBooks = [
+            new Book('Book 1', 'Author 1', '2012', '012345'),
+            new Book('Book 2', 'Author 2', '1989', '123456')
+        ];
+
+        $this->bookRepository
+            ->expects($this->once())
+            ->method('findAvailableBooks')
+            ->willReturn($availableBooks);
+
+        $result = $this->sut->__invoke();
+
+        $this->assertSame($availableBooks, $result);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bookRepository = $this->createMock(BookRepository::class);
+        $this->sut = new ListAvailableBooksAction($this->bookRepository);
+    }
+}
