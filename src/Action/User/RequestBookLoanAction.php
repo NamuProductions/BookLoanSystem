3643ec65596856f0113be76b9b26be30858d281b
ryan <tvmuixi@hotmail.com>
2024-05-29 18:46:45 +0100

    actualizado para que pase primer test

--- src/Action/User/RequestBookLoanAction.php
+++ src/Action/User/RequestBookLoanAction.php
@@ -2,9 +2,11 @@
 
 namespace App\Action\User;
 
+use App\Domain\Model\Loan;
 use App\Domain\Repository\BookRepository;
 use App\Domain\Repository\LoanRepository;
 use App\Domain\Repository\UserRepository;
+use InvalidArgumentException;
 
 readonly class RequestBookLoanAction
 {
@@ -14,7 +16,26 @@ readonly class RequestBookLoanAction
         private UserRepository $userRepository
     ) {}
 
-    public function __invoke(string $userId, string $bookId): void
+    public function __invoke(string $userName, string $bookId): void
     {
+        $user = $this->userRepository->findByUserName($userName);
+        if (!$user) {
+            throw new InvalidArgumentException('User not found.');
+        }
+
+        $book = $this->bookRepository->findByIdNumber($bookId);
+        if (!$book) {
+            throw new InvalidArgumentException('Book not found.');
+        }
+
+        if (!$book->isAvailable()) {
+            throw new InvalidArgumentException('Book is not available for loan.');
+        }
+
+        $loan = new Loan($user->getUserName(), $book->getIdNumber(), date('Y-m-d'), '');
+        $this->loanRepository->save($loan);
+
+        $book->markAsUnavailable();
+        $this->bookRepository->save($book);
     }
 }
