a9da3c7cb2b45187f81150cc878ad591259f07fc
ryan <tvmuixi@hotmail.com>
2024-05-29 16:26:42 +0100

    creamos la clase SearchBooksAction.php

---
+++ src/Action/User/SearchBooksAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Action\User;
+
+use App\Domain\Repository\BookRepository;
+
+readonly class SearchBooksAction
+{
+public function __construct(private BookRepository $bookRepository)
+{
+
+}
+    public function __invoke(string $query): array
+    {
+        return $this->bookRepository->search($query);
+    }
+}
