059222a5d9f88c02899e3a323e58b2666963422c
ryan <tvmuixi@hotmail.com>
2024-05-29 17:24:58 +0100

    RequestBookLoanAction starting

--- src/Action/User/RequestBookLoanAction.php
+++ src/Action/User/RequestBookLoanAction.php
@@ -2,7 +2,19 @@
 
 namespace App\Action\User;
 
-class RequestBookLoanAction
-{
+use App\Domain\Repository\BookRepository;
+use App\Domain\Repository\LoanRepository;
+use App\Domain\Repository\UserRepository;
 
+readonly class RequestBookLoanAction
+{
+    public function __construct(
+        private BookRepository $bookRepository,
+        private LoanRepository $loanRepository,
+        private UserRepository $userRepository
+    ) {}
+
+    public function __invoke(string $userId, string $bookId): void
+    {
+    }
 }
