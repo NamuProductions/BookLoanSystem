8f63cab9397ceaa9a996cc032403eb4bbf1c65db
ryan <tvmuixi@hotmail.com>
2024-05-31 01:51:10 +0100

    class filed

--- src/Action/Admin/ListReturnRequestsAction.php
+++ src/Action/Admin/ListReturnRequestsAction.php
@@ -2,7 +2,21 @@
 
 namespace App\Action\Admin;
 
-class ListReturnRequestsAction
-{
+use App\Domain\Repository\LoanRepository;
+use Exception;
 
+readonly class ListReturnRequestsAction
+{
+    public function __construct(private LoanRepository $loanRepository)
+    {
+    }
+
+    public function __invoke(): array
+    {
+        try {
+            return $this->loanRepository->findAllReturnRequests();
+        } catch (Exception) {
+            throw new Exception('Error retrieving return requests');
+        }
+    }
 }
