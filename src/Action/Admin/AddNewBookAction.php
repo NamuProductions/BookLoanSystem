44b210a15e5567f99762e8bbf2fdc1f0ab7e5298
ryan <tvmuixi@hotmail.com>
2024-06-05 19:03:11 +0100

    Year

--- src/Action/Admin/AddNewBookAction.php
+++ src/Action/Admin/AddNewBookAction.php
@@ -6,6 +6,7 @@ namespace App\Action\Admin;
 
 use App\Domain\Model\Book;
 use App\Domain\Repository\BookRepository;
+use App\Domain\ValueObject\Year;
 use PHPUnit\Event\InvalidArgumentException;
 
 readonly class AddNewBookAction
@@ -15,7 +16,7 @@ readonly class AddNewBookAction
     {
     }
 
-    public function __invoke(string $title, string $author, string $year, string $idNumber): void
+    public function __invoke(string $title, string $author, Year $year, string $idNumber): void
     {
         if (empty($title)) {
             throw new InvalidArgumentException('Title is required');
@@ -23,17 +24,11 @@ readonly class AddNewBookAction
         if (empty($author)) {
             throw new InvalidArgumentException('Author is required');
         }
-        if (empty($year)) {
-            throw new InvalidArgumentException('Year is required');
-        }
+
         if (empty($idNumber)) {
             throw new InvalidArgumentException('IdNumber is required');
         }
-        $currentYear = (int)date('Y');
-        $yearInt = (int)$year;
-        if ($yearInt < 0 || $yearInt > $currentYear || (string)$yearInt !== $year) {
-            throw new InvalidArgumentException('Year must be an integer between 0 and the current year.');
-        }
+
         $book = new Book($title, $author, $year, $idNumber);
         $this->bookRepository->save($book);
     }
