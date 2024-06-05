61f1f29dc6168180b9c7a44d9747da7ca0f41e85
ryan <tvmuixi@hotmail.com>
2024-06-05 19:12:55 +0100

    Year modifications in tests

--- Tests/Action/Admin/ListReturnRequestsActionTest.php
+++ Tests/Action/Admin/ListReturnRequestsActionTest.php
@@ -6,6 +6,7 @@ namespace Action\Admin;
 use App\Action\Admin\ListReturnRequestsAction;
 use App\Domain\Model\Book;
 use App\Domain\Repository\BookRepository;
+use App\Domain\ValueObject\Year;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
@@ -22,11 +23,11 @@ class ListReturnRequestsActionTest extends TestCase
         $borrowDate2 = new DateTime('2023-02-01');
         $returnDate2 = new DateTime('2023-02-12');
 
-        $book1 = new Book('Test Title', 'Test Author', '2021', 'book1');
+        $book1 = new Book('Test Title', 'Test Author', new Year(2021), 'book1');
         $loan1 = $book1->borrow('user1', $borrowDate1);
         $loan1->markAsReturned($returnDate1);
 
-        $book2 = new Book('Test Title', 'Test Author', '2021', 'book2');
+        $book2 = new Book('Test Title', 'Test Author', new Year(2021), 'book2');
         $loan2 = $book2->borrow('user2', $borrowDate2);
         $loan2->markAsReturned($returnDate2);
 
