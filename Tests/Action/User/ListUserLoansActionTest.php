fdd0c2a9f51be262e3648262af9cae0be76c57b8
ryan <tvmuixi@hotmail.com>
2024-06-05 19:14:41 +0100

    Year modifications in tests

--- Tests/Action/User/ListUserLoansActionTest.php
+++ Tests/Action/User/ListUserLoansActionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Action\User;
 
 use App\Domain\Model\Book;
+use App\Domain\ValueObject\Year;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
@@ -12,7 +13,7 @@ class ListUserLoansActionTest extends TestCase
     public function test_it_should_return_user_loans(): void
     {
         $borrowDate = new DateTime('2023-01-01');
-        $book = new Book('Test Title', 'Test Author', '2021', 'book1');
+        $book = new Book('Test Title', 'Test Author', new Year(2021), 'book1');
         $book->borrow('user1', $borrowDate);
 
         $loans = $book->findByUser('user1');
@@ -26,7 +27,7 @@ class ListUserLoansActionTest extends TestCase
 
     public function test_it_should_return_empty_list_when_user_has_no_loans(): void
     {
-        $book = new Book('Test Title', 'Test Author', '2021', 'book1');
+        $book = new Book('Test Title', 'Test Author', new Year(2021), 'book1');
         $loans = $book->findByUser('user1');
 
         $this->assertEmpty($loans);
@@ -35,7 +36,7 @@ class ListUserLoansActionTest extends TestCase
     public function test_it_should_mark_book_as_returned(): void
     {
         $borrowDate = new DateTime('2023-01-01');
-        $book = new Book('Test Title', 'Test Author', '2021', 'book1');
+        $book = new Book('Test Title', 'Test Author', new Year(2021), 'book1');
         $book->borrow('user1', $borrowDate);
 
         $loans = $book->findByUser('user1');
