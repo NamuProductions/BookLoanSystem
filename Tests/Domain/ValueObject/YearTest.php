0b0bce3b615e3d3f41c07d8683de8b8208c13787
ryan <tvmuixi@hotmail.com>
2024-06-06 12:19:49 +0100

    para más checks assertInstanceOf

--- Tests/Domain/ValueObject/YearTest.php
+++ Tests/Domain/ValueObject/YearTest.php
@@ -10,6 +10,7 @@ class YearTest extends TestCase
     public function test_it_should_create_valid_year_in_Anno_Domini()
     {
         $year = new Year(2023);
+        $this->assertInstanceOf(Year::class, $year);
         $this->assertEquals(2023, $year->getValue());
         $this->assertEquals("2023", (string)$year);
     }
