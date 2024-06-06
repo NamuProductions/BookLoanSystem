6a19bdab444fbd08bd99a4021e2fffb080112e1a
ryan <tvmuixi@hotmail.com>
2024-06-06 12:14:38 +0100

    test en año Era Actual

--- Tests/Domain/ValueObject/YearTest.php
+++ Tests/Domain/ValueObject/YearTest.php
@@ -7,5 +7,10 @@ use PHPUnit\Framework\TestCase;
 
 class YearTest extends TestCase
 {
-
+    public function test_it_should_create_valid_year_in_Anno_Domini()
+    {
+        $year = new Year(2023);
+        $this->assertEquals(2023, $year->getValue());
+        $this->assertEquals("2023", (string)$year);
+    }
 }
