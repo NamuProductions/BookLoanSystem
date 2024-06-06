547741c5ffa28e564aaf55db3097e270573f36b8
ryan <tvmuixi@hotmail.com>
2024-06-06 12:40:48 +0100

    ajustando mejor los nombres de los tests

--- Tests/Domain/ValueObject/YearTest.php
+++ Tests/Domain/ValueObject/YearTest.php
@@ -23,7 +23,7 @@ class YearTest extends TestCase
         $this->assertEquals('500B.C.', (string)$year);
     }
 
-    public function test_year_case_zero_should_return_0_as_string(): void
+    public function test_it_should_return_0_as_string_is_case_0_year(): void
     {
         $year = new Year(0);
         $this->assertInstanceOf(Year::class, $year);
