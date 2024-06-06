5ef080821f022930accec2590e6605f32b6c02b2
ryan <tvmuixi@hotmail.com>
2024-06-06 12:38:49 +0100

    ajustando mejor los nombres de los tests

--- Tests/Domain/ValueObject/YearTest.php
+++ Tests/Domain/ValueObject/YearTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class YearTest extends TestCase
 {
-    public function test_it_should_create_valid_year_in_Anno_Domini()
+    public function test_it_should_return_any_year_as_string_when_year_is_greater_than_zero()
     {
         $year = new Year(2023);
         $this->assertInstanceOf(Year::class, $year);
