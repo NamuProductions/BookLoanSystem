6b6076a80e59a56a291432f1be4f92d53636ac18
ryan <tvmuixi@hotmail.com>
2024-06-06 12:17:10 +0100

    aceptamos cualquier año como válido, pk no?

--- src/Domain/ValueObject/Year.php
+++ src/Domain/ValueObject/Year.php
@@ -3,30 +3,17 @@ declare(strict_types=1);
 
 namespace App\Domain\ValueObject;
 
-use InvalidArgumentException;
 
 class Year
 {
     private int $year;
-    private const MIN_YEAR_BC = -10000;
-    private const MAX_YEAR_AD = 10000;
-    private const YEAR_ZERO = 0;
 
     public function __construct(int $year)
     {
-        $this->validateYear($year);
         $this->year = $year;
     }
 
-    private function validateYear(int $year): void
-    {
-        $currentYear = (int)date("Y");
-        if ($year == self::YEAR_ZERO || $year < self::MIN_YEAR_BC || $year > ($currentYear + self::MAX_YEAR_AD)) {
-            throw new InvalidArgumentException('Invalid Year');
-        }
-    }
-
-    public function getValue(): int
+        public function getValue(): int
     {
         return $this->year;
     }
