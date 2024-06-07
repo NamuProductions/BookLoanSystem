b4c66711bd165af2c5774a9f3a32841838583a3e
ryan <tvmuixi@hotmail.com>
2024-06-07 11:14:24 +0100

    modificado para que use la consulta hecha por loanrequestService, separando consulta de la acción

--- src/Action/Admin/ListLoanRequestsAction.php
+++ src/Action/Admin/ListLoanRequestsAction.php
@@ -3,24 +3,19 @@ declare(strict_types=1);
 
 namespace App\Action\Admin;
 
-use App\Domain\Repository\BookRepository;
+use App\Service\LoanRequestQueryService;
 use Exception;
 
 readonly class ListLoanRequestsAction
 {
-    public function __construct(private BookRepository $bookRepository)
-    {
-    }
+    public function __construct(
+        private LoanRequestQueryService $loanRequestQueryService)
+    {}
 
     public function __invoke(): array
     {
         try {
-            $allBooks = $this->bookRepository->findAll();
-            $loanRequests = [];
-            foreach ($allBooks as $book) {
-                $loanRequests = array_merge($loanRequests, $book->notReturnedLoans());
-            }
-            return $loanRequests;
+            return $this->loanRequestQueryService->getAllLoanRequests();
         } catch (Exception) {
             throw new Exception('Error retrieving loan requests');
         }
