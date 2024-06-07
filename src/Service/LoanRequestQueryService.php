938da49e2bd7b662215be9146af33c21e3b7aafa
ryan <tvmuixi@hotmail.com>
2024-06-07 11:11:24 +0100

    creado query para solo hacer consultas y no modificar nada $loanRequests

---
+++ src/Service/LoanRequestQueryService.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Domain\Repository\BookRepository;
+
+class LoanRequestQueryService
+{
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
+    public function getAllLoanRequests(): array
+    {
+        $allBooks = $this->bookRepository->findAll();
+        $loanRequests = [];
+        foreach ($allBooks as $book) {
+            $loanRequests = array_merge($loanRequests, $book->notReturnedLoans());
+        }
+        return $loanRequests;
+    }
+}
