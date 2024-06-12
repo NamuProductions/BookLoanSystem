0b416966dcfb9381d58b5b360af37715fdebb403
ryan <tvmuixi@hotmail.com>
2024-06-12 11:58:58 +0100

    Created MySqlLoanRequestQueryService.php

--- src/Service/LoanRequestQueryService.php
+++ src/Service/LoanRequestQueryService.php
@@ -1,23 +1,23 @@
 <?php
-declare(strict_types=1);
-
-namespace App\Service;
-
-use App\Domain\Repository\BookRepository;
-
-readonly class LoanRequestQueryService implements LoanRequestQueryServiceInterface
-{
-    public function __construct(private BookRepository $bookRepository)
-    {
-    }
-
-    public function allLoanRequests(): array
-    {
-        $allBooks = $this->bookRepository->findAll();
-        $loanRequests = [];
-        foreach ($allBooks as $book) {
-            $loanRequests = array_merge($loanRequests, $book->notReturnedLoans());
-        }
-        return $loanRequests;
-    }
-}
+//declare(strict_types=1);
+//
+//namespace App\Service;
+//
+//use App\Domain\Repository\BookRepository;
+//
+//readonly class LoanRequestQueryService implements LoanRequestQueryServiceInterface
+//{
+//    public function __construct(private BookRepository $bookRepository)
+//    {
+//    }
+//
+//    public function allLoanRequests(): array
+//    {
+//        $allBooks = $this->bookRepository->findAll();
+//        $loanRequests = [];
+//        foreach ($allBooks as $book) {
+//            $loanRequests = array_merge($loanRequests, $book->notReturnedLoans());
+//        }
+//        return $loanRequests;
+//    }
+//}
---
+++ src/Service/MySqlLoanRequestQueryService.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Action\Admin\LoanRequestsAction;
+use PDO;
+
+readonly class MySqlLoanRequestQueryService implements LoanRequestQueryServiceInterface
+{
+    public function __construct(private \PDO $databaseConnection)
+    {
+    }
+
+    public function allLoanRequests(): array
+    {
+        $statement = $this->databaseConnection->prepare('SELECT book_id, title, user_name, user_id, borrow_at FROM loan_requests
+                                                join user on user.id = loan_requests.userId
+                                                join book on book.id = loan_requests.bookId
+                                                where status = "pending"'
+        );
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_FUNC, function($bookId, $title, $userName, $userId, $borrowedAtDateString){
+            return new LoanRequestsDto($bookId, $title, $userName, $userId, new \DateTime($borrowedAtDateString));
+        });
+    }
+}
