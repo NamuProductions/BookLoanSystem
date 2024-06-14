521de0010cf395ed1ea7a22f8729db279e2f141e
ryan <tvmuixi@hotmail.com>
2024-06-14 20:34:34 +0100

    borrar y rollback xD

--- src/Service/ReturnRequestQueryService.php
+++ src/Service/ReturnRequestQueryService.php
@@ -1,23 +1,23 @@
 <?php
-//declare(strict_types=1);
-//
-//namespace App\Service;
-//
-//use App\Domain\Repository\BookRepository;
-//
-//readonly class ReturnRequestQueryService implements ReturnRequestQueryServiceInterface
-//{
-//    public function __construct(private BookRepository $bookRepository)
-//    {
-//    }
-//
-//    public function returnRequests(): array
-//    {
-//        $books = $this->bookRepository->findAll();
-//        $returnRequests = [];
-//        foreach ($books as $book) {
-//            $returnRequests = array_merge($returnRequests, $book->returnedLoans());
-//        }
-//        return $returnRequests;
-//    }
-//}
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Domain\Repository\BookRepository;
+
+readonly class ReturnRequestQueryService implements ReturnRequestQueryServiceInterface
+{
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
+    public function returnRequests(): array
+    {
+        $books = $this->bookRepository->findAll();
+        $returnRequests = [];
+        foreach ($books as $book) {
+            $returnRequests = array_merge($returnRequests, $book->returnedLoans());
+        }
+        return $returnRequests;
+    }
+}
