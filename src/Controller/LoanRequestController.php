88208ace5b4a8d2bef3749c4da43d4b25b643b4c
ryan <tvmuixi@hotmail.com>
2024-06-17 17:42:13 +0100

    ajustado controller sin hablar con MySql directamente

--- src/Controller/LoanRequestController.php
+++ src/Controller/LoanRequestController.php
@@ -3,25 +3,24 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Service\DatabaseService;
-use App\Service\MySqlLoanRequestQueryService;
-use App\Service\LoanRequestDto;
+use App\Action\Admin\LoanRequestsAction;
+use Exception;
 
 class LoanRequestController
 {
-    public function index(): void
+    private LoanRequestsAction $loanRequestsAction;
+
+    public function __construct(LoanRequestsAction $loanRequestsAction)
     {
-        $databaseService = new DatabaseService();
-        $pdo = $databaseService->getDatabaseConnection();
+        $this->loanRequestsAction = $loanRequestsAction;
+    }
 
-        $loanRequestService = new MySqlLoanRequestQueryService($pdo);
-
-        $loanRequests = $loanRequestService->allLoanRequests();
-
-        foreach ($loanRequests as $loanRequest) {
-            $returnDate = $loanRequest->returnDate ? $loanRequest->returnDate->format('Y-m-d') : 'Not Returned';
-            echo "ID: $loanRequest->bookId, Book Title: $loanRequest->title, User Name: $loanRequest->userName, Borrowed At: {$loanRequest->borrowedAt->format('Y-m-d')}, Return Date: $returnDate";
-            echo "<br>";
+    public function index(): array
+    {
+        try {
+            return $this->loanRequestsAction();
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
         }
     }
 }
