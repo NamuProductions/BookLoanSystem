23cdc50ef283a86c47ac21af07a510b34c93a911
ryan <tvmuixi@hotmail.com>
2024-06-17 12:31:30 +0100

    añadida la lógica de loanRequestAction en MySql

--- src/Service/MySqlLoanRequestQueryService.php
+++ src/Service/MySqlLoanRequestQueryService.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace App\Service;
 
 use DateTime;
+use Exception;
 use PDO;
+use PDOException;
 
 class MySqlLoanRequestQueryService implements LoanRequestQueryServiceInterface
 {
@@ -17,27 +19,31 @@ class MySqlLoanRequestQueryService implements LoanRequestQueryServiceInterface
 
     public function allLoanRequests(): array
     {
-        $statement = $this->databaseConnection->prepare("SELECT lr.book_id, b.title, u.user_name, u.id AS user_id, lr.borrow_at, lr.return_at
-                                                        FROM loan_requests lr
-                                                          JOIN users u ON lr.user_id = u.id
-                                                          JOIN books b ON lr.book_id = b.id
-                                                          WHERE lr.status = 'pending'");
-        $statement->execute();
-        $loanRequests = [];
+        try {
+            $statement = $this->databaseConnection->prepare("SELECT lr.book_id, b.title, u.user_name, u.id AS user_id, lr.borrow_at, lr.return_at
+                                                            FROM loan_requests lr
+                                                            JOIN users u ON lr.user_id = u.id
+                                                            JOIN books b ON lr.book_id = b.id
+                                                            WHERE lr.status = 'pending'");
+            $statement->execute();
+            $loanRequests = [];
 
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-            $borrowedAt = new DateTime($row['borrow_at']);
-            $returnAt = $row['return_at'] ? new DateTime($row['return_at']) : null;
-            $loanRequests[] = new LoanRequestDto(
-                (string)$row['book_id'],
-                (string)$row['title'],
-                (string)$row['user_name'],
-                (string)$row['user_id'],
-                $borrowedAt,
-                $returnAt
-            );
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                $borrowedAt = new DateTime($row['borrow_at']);
+                $returnAt = $row['return_at'] ? new DateTime($row['return_at']) : null;
+                $loanRequests[] = new LoanRequestDto(
+                    (string)$row['book_id'],
+                    (string)$row['title'],
+                    (string)$row['user_name'],
+                    (string)$row['user_id'],
+                    $borrowedAt,
+                    $returnAt
+                );
+            }
+
+            return $loanRequests;
+        } catch (PDOException) {
+            throw new Exception('Error retrieving loan requests');
         }
-
-        return $loanRequests;
     }
 }
