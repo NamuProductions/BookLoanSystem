3738af1fcc21e347e20a0bad979c02447d087840
ryan <tvmuixi@hotmail.com>
2024-06-25 12:50:03 +0100

    ListUserLoansActionTest.php corrected

--- Tests/Action/User/ListUserLoansActionTest.php
+++ Tests/Action/User/ListUserLoansActionTest.php
@@ -13,6 +13,8 @@ class ListUserLoansActionTest extends TestCase
     public function test_it_should_return_user_loans(): void
     {
         $borrowDate = new DateTime('2023-01-01');
+        $dueDate = (clone $borrowDate)->modify('+14 days');
+
         $book = new Book('Test Title', 'Test Author', 'Català', new Year(2021), 'book1');
         $book->borrow('user1', $borrowDate);
 
@@ -21,8 +23,8 @@ class ListUserLoansActionTest extends TestCase
         $this->assertCount(1, $loans);
         $this->assertSame('user1', $loans[0]->getUserId());
         $this->assertSame($book->bookId(), $loans[0]->getBookId());
-        $this->assertEquals($borrowDate->format('Y-m-d'), $loans[0]->getBorrowDate()->format('Y-m-d'));
-        $this->assertEquals($borrowDate->modify('+14 days')->format('Y-m-d'), $loans[0]->getDueDate()->format('Y-m-d'));
+        $this->assertEquals($borrowDate->format('Y-m-d'), $loans[0]->getDateRange()->start()->format('Y-m-d'));
+        $this->assertEquals($dueDate->format('Y-m-d'), $loans[0]->getDateRange()->end()->format('Y-m-d'));
     }
 
     public function test_it_should_return_empty_list_when_user_has_no_loans(): void
@@ -36,6 +38,7 @@ class ListUserLoansActionTest extends TestCase
     public function test_it_should_mark_book_as_returned(): void
     {
         $borrowDate = new DateTime('2023-01-01');
+
         $book = new Book('Test Title', 'Test Author', 'Català', new Year(2021), 'book1');
         $book->borrow('user1', $borrowDate);
 
@@ -48,6 +51,6 @@ class ListUserLoansActionTest extends TestCase
         $loans = $book->findAllLoansByUser('user1');
         $this->assertCount(1, $loans);
         $this->assertTrue($loans[0]->isReturned());
-        $this->assertInstanceOf(DateTime::class, $loans[0]->returnDate());
+        $this->assertInstanceOf(DateTime::class, $loans[0]->realReturnDate());
     }
 }
