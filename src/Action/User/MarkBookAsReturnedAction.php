64a24757826cd25873bf8d8c43311d5fb95b9744
ryan <tvmuixi@hotmail.com>
2024-06-25 13:24:40 +0100

    implementado check si es active loan o no y sino lanza exception

--- src/Action/User/MarkBookAsReturnedAction.php
+++ src/Action/User/MarkBookAsReturnedAction.php
@@ -4,17 +4,30 @@ declare(strict_types=1);
 namespace App\Action\User;
 
 use App\Domain\Repository\BookRepository;
+use InvalidArgumentException;
 
 readonly class MarkBookAsReturnedAction
 {
     public function __construct(
-        private BookRepository $bookRepository,
+        private BookRepository $bookRepository
     ) {}
 
     public function __invoke(string $userId, string $bookId): void
     {
         $book = $this->bookRepository->findById($bookId);
+
+        if ($book === null) {
+            throw new InvalidArgumentException('Book not found.');
+        }
+
+        $activeLoan = $book->findActiveLoanByUser($userId);
+
+        if ($activeLoan === null) {
+            throw new InvalidArgumentException('No active loan found for this book and user.');
+        }
+
         $book->returnBook($userId);
+
         $this->bookRepository->save($book);
     }
 }
