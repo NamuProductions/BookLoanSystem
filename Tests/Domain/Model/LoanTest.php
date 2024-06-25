21e0fbdcc0671d682ce9e4411cd94f1d61e2d090
ryan <tvmuixi@hotmail.com>
2024-06-25 13:00:35 +0100

    hago public LOAN_DAYS para usar siempre el mismo

--- Tests/Domain/Model/LoanTest.php
+++ Tests/Domain/Model/LoanTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Domain\Model;
 
 use App\Domain\Model\Loan;
+use App\Domain\ValueObject\DateRange;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
@@ -11,15 +12,15 @@ class LoanTest extends TestCase
 {
     public function test_it_should_loan_creation_and_properties(): void
     {
-        $borrowDate = new DateTime('2023-01-01');
-        $dueDate = new DateTime('2023-01-15');
+        $start = new DateTime('2023-01-01');
+        $lastDayOfLoan = new DateTime('2023-01-15');
+        $dateRange = new DateRange($start,$lastDayOfLoan);
 
-        $loan = new Loan('book1', 'user1', $borrowDate, $dueDate);
+        $loan = new Loan('book1', 'user1', $dateRange);
 
         $this->assertSame('book1', $loan->getBookId());
         $this->assertSame('user1', $loan->getUserId());
-        $this->assertEquals($borrowDate, $loan->getBorrowDate());
-        $this->assertEquals($dueDate, $loan->getDueDate());
+        $this->assertEquals($dateRange, $loan->dateRange());
         $this->assertFalse($loan->isReturned());
     }
 }
