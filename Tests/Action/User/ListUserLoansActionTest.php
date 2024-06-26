8729b94e5fd1b03f8158f47e785169e9f6fbce91
ryan <tvmuixi@hotmail.com>
2024-06-26 11:00:30 +0100

    modificado ListUserLoansActionTest.php

--- Tests/Action/User/ListUserLoansActionTest.php
+++ Tests/Action/User/ListUserLoansActionTest.php
@@ -3,22 +3,31 @@ declare(strict_types=1);
 
 namespace Action\User;
 
+use App\Action\User\ListUserLoansAction;
 use App\Domain\Model\Book;
+use App\Domain\Repository\BookRepository;
 use App\Domain\ValueObject\Year;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class ListUserLoansActionTest extends TestCase
 {
+    private BookRepository $bookRepository;
+    private ListUserLoansAction $sut;
+
     public function test_it_should_return_user_loans(): void
     {
         $borrowDate = new DateTime('2023-01-01');
         $dueDate = (clone $borrowDate)->modify('+14 days');
-
         $book = new Book('Test Title', 'Test Author', 'Català', new Year(2021), 'book1');
         $book->borrow('user1', $borrowDate);
 
-        $loans = $book->findAllLoansByUser('user1');
+        $this->bookRepository->expects($this->once())
+            ->method('findAllLoansByUser')
+            ->with('user1')
+            ->willReturn([$book]);
+
+        $loans = $this->sut->execute('user1');
 
         $this->assertCount(1, $loans);
         $this->assertSame('user1', $loans[0]->getUserId());
@@ -29,9 +38,20 @@ class ListUserLoansActionTest extends TestCase
 
     public function test_it_should_return_empty_list_when_user_has_no_loans(): void
     {
-        $book = new Book('Test Title', 'Test Author', 'Català', new Year(2021), 'book1');
-        $loans = $book->findAllLoansByUser('user1');
+        $this->bookRepository->expects($this->once())
+            ->method('findAllLoansByUser')
+            ->with('user1')
+            ->willReturn([]);
+
+        $loans = $this->sut->execute('user1');
 
         $this->assertEmpty($loans);
     }
-   }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->bookRepository = $this->createMock(BookRepository::class);
+        $this->sut = new ListUserLoansAction($this->bookRepository);
+    }
+}
