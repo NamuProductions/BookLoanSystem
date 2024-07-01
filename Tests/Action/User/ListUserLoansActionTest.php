2af0c1b851dcd368a66b9061c295eb8237a725c2
ryan <tvmuixi@hotmail.com>
2024-07-01 12:43:30 +0100

    corregimos los tests

--- Tests/Action/User/ListUserLoansActionTest.php
+++ Tests/Action/User/ListUserLoansActionTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace Action\User;
 
 use App\Action\User\ListUserLoansAction;
-use App\Domain\Model\Book;
+use App\Domain\Model\Loan;
 use App\Domain\Repository\BookRepository;
-use App\Domain\ValueObject\Year;
+use App\Domain\ValueObject\LoansDateTimes;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 
@@ -18,37 +18,34 @@ class ListUserLoansActionTest extends TestCase
     public function test_it_should_return_user_loans(): void
     {
         $borrowDate = new DateTime('2023-01-01');
-        $dueDate = (clone $borrowDate)->modify('+14 days');
+        $loansDateTimes = new LoansDateTimes($borrowDate);
 
-        $book1 = new Book('Test Title 1', 'Test Author 1', 'Català', new Year(2021), 'book1');
-        $book1->borrow('user1', $borrowDate);
-
-        $book2 = new Book('Test Title 2', 'Test Author 2', 'English', new Year(2022), 'book2');
-        $book2->borrow('user1', $borrowDate);
+        $loan1 = new Loan('book1', 'user1', $loansDateTimes);
+        $loan2 = new Loan('book2', 'user1', $loansDateTimes);
 
         $this->bookRepository->expects($this->once())
-            ->method('findAll')
-            ->willReturn([$book1, $book2]);
+            ->method('findAllLoansByUser')
+            ->willReturn([$loan1, $loan2]);
 
         $loans = ($this->sut)('user1');
 
         $this->assertCount(2, $loans);
 
         $this->assertSame('user1', $loans[0]->getUserId());
-        $this->assertSame($book1->bookId(), $loans[0]->getBookId());
+        $this->assertSame('book1', $loans[0]->getBookId());
         $this->assertEquals($borrowDate->format('Y-m-d'), $loans[0]->loansDateTimes()->loanBorrowedAt()->format('Y-m-d'));
-        $this->assertEquals($dueDate->format('Y-m-d'), $loans[0]->loansDateTimes()->loanMaximumReturnDate()->format('Y-m-d'));
+        $this->assertEquals($loansDateTimes->LoanMaximumReturnDate()->format('Y-m-d'), $loans[0]->loansDateTimes()->LoanMaximumReturnDate()->format('Y-m-d'));
 
         $this->assertSame('user1', $loans[1]->getUserId());
-        $this->assertSame($book2->bookId(), $loans[1]->getBookId());
+        $this->assertSame('book2', $loans[1]->getBookId());
         $this->assertEquals($borrowDate->format('Y-m-d'), $loans[1]->loansDateTimes()->loanBorrowedAt()->format('Y-m-d'));
-        $this->assertEquals($dueDate->format('Y-m-d'), $loans[1]->loansDateTimes()->loanMaximumReturnDate()->format('Y-m-d'));
+        $this->assertEquals($loansDateTimes->LoanMaximumReturnDate()->format('Y-m-d'), $loans[1]->loansDateTimes()->LoanMaximumReturnDate()->format('Y-m-d'));
     }
 
     public function test_it_should_return_empty_list_when_user_has_no_loans(): void
     {
         $this->bookRepository->expects($this->once())
-            ->method('findAll')
+            ->method('findAllLoansByUser')
             ->willReturn([]);
 
         $loans = ($this->sut)('user1');
