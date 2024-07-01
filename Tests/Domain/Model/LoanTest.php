0ce57af7ceeed91aab331664552b78057d6b2a96
ryan <tvmuixi@hotmail.com>
2024-07-01 10:55:50 +0100

    uso de SUT para los tests y ampliados

--- Tests/Domain/Model/LoanTest.php
+++ Tests/Domain/Model/LoanTest.php
@@ -10,16 +10,36 @@ use PHPUnit\Framework\TestCase;
 
 class LoanTest extends TestCase
 {
+    private Loan $sut;
+
     public function test_it_should_loan_creation_and_properties(): void
     {
+        $this->assertSame('book1', $this->sut->getBookId());
+        $this->assertSame('user1', $this->sut->getUserId());
+        $this->assertFalse($this->sut->isReturned());
+    }
+
+    public function test_it_should_mark_loan_as_returned(): void
+    {
+        $returnDate = new DateTime('2023-01-10');
+        $this->sut->markAsReturned($returnDate);
+
+        $this->assertTrue($this->sut->isReturned());
+        $this->assertSame($returnDate, $this->sut->loanReturnedAt());
+    }
+
+    public function test_it_should_return_null_if_loan_not_returned(): void
+    {
+        $this->assertNull($this->sut->loanReturnedAt());
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
         $borrowDate = new DateTime('2023-01-01');
         $loansDateTimes = new LoansDateTimes($borrowDate);
 
-        $loan = new Loan('book1', 'user1', $loansDateTimes);
-
-        $this->assertSame('book1', $loan->getBookId());
-        $this->assertSame('user1', $loan->getUserId());
-        $this->assertEquals($loansDateTimes, $loan->loansDateTimes());
-        $this->assertFalse($loan->isReturned());
+        $this->sut = new Loan('book1', 'user1', $loansDateTimes);
     }
 }
