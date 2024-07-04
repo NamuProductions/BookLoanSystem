1382563830b22fe2ffdb6f34758f696ebd00bf82
ryan <tvmuixi@hotmail.com>
2024-07-04 11:06:20 +0100

    pagina home para register o login

---
+++ src/View/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+<h1>Welcome to Book Loan System</h1>
+<p><a href="/register">Register</a></p>
+<p><a href="/login">Login</a></p>
+</body>
+</html>
