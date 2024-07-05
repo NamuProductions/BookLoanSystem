afce9833fa38a01e2039a21bd6b316290b8f9180
ryan <tvmuixi@hotmail.com>
2024-07-05 19:29:12 +0100

    corregido BookRepository.php

--- src/Domain/Repository/BookRepository.php
+++ src/Domain/Repository/BookRepository.php
@@ -15,7 +15,7 @@ interface BookRepository
 
     public function search(string $query): array;
 
-    public function findById(string $id): ?Book;
+    public function findById(int $bookId): ?Book;
 
-    public function findAllLoansByUser(string $userId): array;
+    public function findAllLoansByUser(int $userId): array;
 }
