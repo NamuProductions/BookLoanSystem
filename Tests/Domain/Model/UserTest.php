b49eba5b51516cde57c7634c49415e07209bd3f8
ryan <tvmuixi@hotmail.com>
2024-07-05 19:02:47 +0100

    corrected user tests

--- Tests/Domain/Model/UserTest.php
+++ Tests/Domain/Model/UserTest.php
@@ -12,27 +12,32 @@ class UserTest extends TestCase
 
     public function test_it_should_return_user_name(): void
     {
-        $this->assertSame('Ryan M', $this->sut->getUserName());
+        $this->assertSame('Ryan M', $this->sut->userName());
     }
 
     public function test_it_should_return_email(): void
     {
-        $this->assertSame('ryan@example.com', $this->sut->getEmail());
+        $this->assertSame('ryan@example.com', $this->sut->email());
     }
 
     public function test_it_should_return_password(): void
     {
-        $this->assertSame('securePassword', $this->sut->getPassword());
+        $this->assertSame('securePassword', $this->sut->password());
     }
 
     public function test_it_should_return_role(): void
     {
-        $this->assertSame('admin', $this->sut->getRole());
+        $this->assertSame('admin', $this->sut->role());
     }
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->sut = new User('Ryan M', 'ryan@example.com', 'securePassword', 'admin');
+        $this->sut = new User(
+            userName: 'Ryan M',
+            password: 'securePassword',
+            email: 'ryan@example.com',
+            role: 'admin'
+        );
     }
 }
