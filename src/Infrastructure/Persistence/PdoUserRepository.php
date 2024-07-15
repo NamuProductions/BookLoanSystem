e21e5a9361e5c624148232d456836384c002abd9
ryan <tvmuixi@hotmail.com>
2024-07-15 16:23:11 +0100

    añadido age en los return del user

--- src/Infrastructure/Persistence/PdoUserRepository.php
+++ src/Infrastructure/Persistence/PdoUserRepository.php
@@ -11,6 +11,7 @@ use PDO;
 class PdoUserRepository implements UserRepository
 {
     private PDO $pdo;
+
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
@@ -18,37 +19,52 @@ class PdoUserRepository implements UserRepository
 
     public function findById(string $userId): ?User
     {
-        $stmt = $this->pdo->prepare('SELECT * FROM library.users WHERE user_id = :userId');
-        $stmt->execute(['userId' => $userId]);
+        $stmt = $this->pdo->prepare('SELECT * FROM users WHERE user_id = :user_id');
+        $stmt->execute(['user_id' => $userId]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($row === false) {
             return null;
         }
 
-        return new User($row['userName'], $row['email'], $row['password'], $row['role'], $row['userId']);
+        return new User(
+            $row['user_id'],
+            $row['user_name'],
+            $row['email'],
+            $row['password'],
+            $row['role'],
+            isset($row['age']) ? (int)$row['age'] : null
+        );
     }
 
     public function save(User $user): void
     {
-        $stmt = $this->pdo->prepare('REPLACE INTO users (user_id, user_name, email, password, role) VALUES (:userId, :userName, :email, :password, :role)');
+        $stmt = $this->pdo->prepare('REPLACE INTO users (user_id, user_name, email, password, role, age) VALUES (:userId, :userName, :email, :password, :role, :age)');
         $stmt->execute([
             'userId' => $user->userId(),
             'userName' => $user->userName(),
             'email' => $user->email(),
             'password' => $user->password(),
             'role' => $user->role(),
+            'age' => $user->age(),
         ]);
     }
 
     public function findByUserName(string $username): ?User
     {
-        $stmt = $this->pdo->prepare('SELECT * FROM users WHERE user_name = :userName');
-        $stmt->execute(['userName' => $username]);
+        $stmt = $this->pdo->prepare('SELECT * FROM users WHERE user_name = :user_name');
+        $stmt->execute(['user_name' => $username]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($row === false) {
             return null;
         }
 
-        return new User($row['userName'], $row['email'], $row['password'], $row['role'], $row['userId']);
+        return new User(
+            $row['user_id'],
+            $row['user_name'],
+            $row['email'],
+            $row['password'],
+            $row['role'],
+            isset($row['age']) ? (int)$row['age'] : null
+        );
     }
 }
