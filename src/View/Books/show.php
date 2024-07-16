a3111486b86971796b7287fa5d76a366de7f1e3c
ryan <tvmuixi@hotmail.com>
2024-07-16 17:09:37 +0100

    added pages and genre

--- src/View/Books/show.php
+++ src/View/Books/show.php
@@ -10,7 +10,10 @@
     <p>Author: <?= $book->author(); ?></p>
     <p>Language: <?= $book->language(); ?></p>
     <p>Year: <?= $book->year()->value(); ?></p>
+    <p>Pages: <?= $book->pages(); ?></p>
+    <p>Genre: <?= $book->genre(); ?></p>
     <form action="/books/<?= $book->bookId(); ?>/borrow" method="POST">
+        <input type="hidden" name="user_id" value="user_id_placeholder">
         <button type="submit" <?= $book->isAvailable() ? '' : 'disabled'; ?>>Borrow</button>
     </form>
     <form action="/books/<?= $book->bookId(); ?>/return" method="POST">
