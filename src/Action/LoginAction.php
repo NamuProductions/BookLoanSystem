6030ccd095c48e252a0118eb51566bce760ff0cf
ryan <tvmuixi@hotmail.com>
2024-07-22 10:47:22 +0100

    hacemos que loginAction retorne usuario si todo es correcto

--- src/Action/LoginAction.php
+++ src/Action/LoginAction.php
@@ -4,24 +4,23 @@ declare(strict_types=1);
 
 namespace App\Action;
 
+use App\Domain\Model\User;
 use App\Domain\Repository\UserRepository;
-use App\Service\SessionManagerInterface;
 use InvalidArgumentException;
 
 readonly class LoginAction
 {
     public function __construct(
         private UserRepository $userRepository,
-        private SessionManagerInterface $sessionManager
     ) {}
 
-    public function __invoke(string $userName, string $password): void
+    public function __invoke(string $userName, string $password): User
     {
         $user = $this->userRepository->findByUserName($userName);
         if (!$user || !password_verify($password, $user->password())) {
             throw new InvalidArgumentException('Invalid username or password.');
         }
 
-        $this->sessionManager->startSession($user);
+        return $user;
     }
 }
