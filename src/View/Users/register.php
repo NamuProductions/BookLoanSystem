817ee69eea172b937ef81b77ffe1c54ea50d913a
ryan <tvmuixi@hotmail.com>
2024-07-30 10:51:22 +0100

    intentando k salgan errores en pantalla

--- src/View/Users/register.php
+++ src/View/Users/register.php
@@ -6,21 +6,31 @@
     <title>Register</title>
 </head>
 <body>
+<h1>Register</h1>
+<?php if (isset($errorMessage)) : ?>
+    <div style="color: red;"><?= htmlspecialchars($errorMessage) ?></div>
+<?php endif; ?>
 <form action="/register" method="POST">
-    <label for="user_name">User Name:</label>
-    <input type="text" id="user_name" name="user_name" required><br><br>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br><br>
-
-    <label for="full_name">Full Name:</label>
-    <input type="text" id="full_name" name="full_name"><br><br>
-
-    <label for="age">Age:</label>
-    <input type="number" id="age" name="age"><br><br>
+    <div>
+        <label for="user_name">Username:</label>
+        <input type="text" id="user_name" name="user_name" value="<?= htmlspecialchars($userName ?? '') ?>">
+    </div>
+    <div>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" value="<?= htmlspecialchars($email ?? '') ?>">
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+    </div>
+    <div>
+        <label for="full_name">Full Name:</label>
+        <input type="text" id="full_name" name="full_name" value="<?= htmlspecialchars($fullName ?? '') ?>">
+    </div>
+    <div>
+        <label for="age">Age:</label>
+        <input type="text" id="age" name="age" value="<?= htmlspecialchars($age ?? '') ?>">
+    </div>
     <button type="submit">Register</button>
 </form>
 </body>
