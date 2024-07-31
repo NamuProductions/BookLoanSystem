7b50c340925079e1624b47f2e0ca5fc422b4c718
ryan <tvmuixi@hotmail.com>
2024-07-31 13:12:52 +0100

    will do vO of age so no need of that here

--- src/Action/User/RegisterUserAction.php
+++ src/Action/User/RegisterUserAction.php
@@ -29,8 +29,6 @@ readonly class RegisterUserAction
         ?string $userId = null,
         ?DateTime $createdAt = null
     ): User {
-        $age = !empty($age) ? $age : null;
-
         $this->validateUserData($userName, $email, $password);
 
         if ($this->userRepository->findByUserName($userName)) {
