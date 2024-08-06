296057d1b8390245ae99d26e1ed5ce4e3508438a
ryan <tvmuixi@hotmail.com>
2024-08-06 12:31:53 +0100

    usando UserName

--- src/Domain/Repository/UserRepository.php
+++ src/Domain/Repository/UserRepository.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Domain\Repository;
 
 use App\Domain\Model\User;
+use App\Domain\ValueObject\UserName;
 
 interface UserRepository
 {
     public function save(User $user): void;
-    public function findByUserName(string $username): ?User;
+    public function findByUserName(UserName $username): ?User;
     public function findById(string $userId): ?User;
 }
